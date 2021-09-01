bc20939f6756038fa36cbdf70d36a0b72baf7476
s1mag1n <57657053+s1mag1n@users.noreply.github.com>
2021-09-01 13:23:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const typeGoods = 'food';
+
+//console.log('https://market.com/'+ typeGoods);
+
+//console.log('https://market.com/'+ typeGoods + '/'+ '10');
+
+console.log(`https://market.com/${typeGoods}`);
+
+
+const games = 'game';
+
+const type = 'type';
+
+console.log(`https://market.com/${games}`);
+
+//
